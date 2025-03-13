0f7b8df5fea1288e2857fa90778ed33b23865053
SAUL_ALEXANDER_JIMENEZ_RUEDA <a.22308051281222@cbtis128.edu.mx>
2025-03-13 18:57:12 +0000

    examen parte 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Refaccion {
+  final String nombre;
+  final int cantidadRefaccion;
+  final double precio;
+  final String calidad;
+  final String marca;
+  final String paisOrigen;
+  final int id;
+
+  Refaccion({
+    required this.nombre,
+    required this.cantidadRefaccion,
+    required this.precio,
+    required this.calidad,
+    required this.marca,
+    required this.paisOrigen,
+    required this.id,
+  });
+}
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  final List<Refaccion> refacciones = [
+    Refaccion(
+        nombre: "Filtro de Aceite",
+        cantidadRefaccion: 10,
+        precio: 250.0,
+        calidad: "Alta",
+        marca: "Bosch",
+        paisOrigen: "Alemania",
+        id: 1),
+    Refaccion(
+        nombre: "Bujía NGK",
+        cantidadRefaccion: 20,
+        precio: 120.0,
+        calidad: "Media",
+        marca: "NGK",
+        paisOrigen: "Japón",
+        id: 2232),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "   Refaccionaria SAJR \n   Saul Jimenez-1222 ",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Color(0xFFFFC8A2), // Naranja pastel
+          centerTitle: true,
+          elevation: 10,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: refacciones
+                .map((refaccion) => Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(24.0),
+                        decoration: BoxDecoration(
+                          color: refaccion.id == 1
+                              ? Color(0xFFF7D794) // Amarillo pastel
+                              : Color(0xFFD4A5A5), // Rojo pastel
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 5,
+                              offset: Offset(2, 2),
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${refaccion.nombre}",
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            SizedBox(height: 10),
+                            Text("Marca: ${refaccion.marca}",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black87)),
+                            Text("Cantidad: ${refaccion.cantidadRefaccion}",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black87)),
+                            Text("Precio: \$${refaccion.precio}",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black87)),
+                            Text("Calidad: ${refaccion.calidad}",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black87)),
+                            Text("País de Origen: ${refaccion.paisOrigen}",
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black87)),
+                          ],
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ),
         ),
       ),
     );
